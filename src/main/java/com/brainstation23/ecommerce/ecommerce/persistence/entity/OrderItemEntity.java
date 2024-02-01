5a24f272aaed711b993d8a02a6f177a2d695d5c2
BISWAAX8 <apurba.biswash@abbvie.com>
2024-02-01 14:06:47 +0600

    Add : db-structure

---
+++ src/main/java/com/brainstation23/ecommerce/ecommerce/persistence/entity/OrderItemEntity.java
@@ -0,0 +1,24 @@
+package com.brainstation23.ecommerce.ecommerce.persistence.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import static com.brainstation23.ecommerce.ecommerce.constant.EntityConstant.ORDER_ITEM_TABLE;
+
+@Entity
+@Getter
+@Setter
+@Accessors(chain = true)
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Table(name = ORDER_ITEM_TABLE)
+public class OrderItemEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
