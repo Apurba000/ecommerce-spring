7391abefe22e4c13e662722d0dbe2edb5268251a
BISWAAX8 <apurba.biswash@abbvie.com>
2024-02-01 20:47:06 +0600

    Add : order user many-to-one order address many-to-one

--- src/main/java/com/brainstation23/ecommerce/ecommerce/persistence/entity/OrderEntity.java
+++ src/main/java/com/brainstation23/ecommerce/ecommerce/persistence/entity/OrderEntity.java
@@ -2,11 +2,10 @@ package com.brainstation23.ecommerce.ecommerce.persistence.entity;
 
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import lombok.experimental.Accessors;
+
+import java.sql.Date;
 
 import static com.brainstation23.ecommerce.ecommerce.constant.EntityConstant.ORDER_TABLE;
 
@@ -21,4 +20,23 @@ public class OrderEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @Column(name = "total_amount")
+    private double totalAmount;
+
+    @Column(name = "order_date")
+    private Date orderDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 50)
+    @NonNull
+    private OrderStatus status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "delivery_address_id")
+    private AddressEntity deliveryAddress;
 }
