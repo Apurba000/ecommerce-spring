1da6b43120eb5b23e2801dd1be0635c5cb5e4495
Moonwar <nishan.rumc@gmail.com>
2024-02-05 19:47:53 +0600

    Categories changes

--- src/main/java/com/brainstation23/ecommerce/ecommerce/service/interfaces/CategoryService.java
+++ src/main/java/com/brainstation23/ecommerce/ecommerce/service/interfaces/CategoryService.java
@@ -3,15 +3,19 @@ package com.brainstation23.ecommerce.ecommerce.service.interfaces;
 import com.brainstation23.ecommerce.ecommerce.model.domain.Category;
 import com.brainstation23.ecommerce.ecommerce.model.dto.category.CategoryCreateRequest;
 import com.brainstation23.ecommerce.ecommerce.model.dto.category.CategoryUpdateRequest;
+import com.brainstation23.ecommerce.ecommerce.persistence.entity.CategoryEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CategoryService {
     Page<Category> getAll(Pageable pageable);
+    List<CategoryEntity> getAllCategory();
     Category getOne(UUID id);
     UUID createOne(CategoryCreateRequest createRequest);
     void updateOne(UUID id, CategoryUpdateRequest updateRequest);
     void deleteOne(UUID id);
+    Category getByName(String name);
 }
