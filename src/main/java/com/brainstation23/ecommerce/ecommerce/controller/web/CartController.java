e7f208011aa1dbddd4621fc8706705f6545d5c86
BISWAAX8 <apurba.biswash@abbvie.com>
2024-02-08 16:59:09 +0600

    Add : Cart Update Delete

--- src/main/java/com/brainstation23/ecommerce/ecommerce/controller/web/CartController.java
+++ src/main/java/com/brainstation23/ecommerce/ecommerce/controller/web/CartController.java
@@ -1,23 +1,22 @@
 package com.brainstation23.ecommerce.ecommerce.controller.web;
 
-
 import com.brainstation23.ecommerce.ecommerce.exception.custom.NotFoundException;
 import com.brainstation23.ecommerce.ecommerce.mapper.CartItemMapper;
 import com.brainstation23.ecommerce.ecommerce.model.domain.User;
+import com.brainstation23.ecommerce.ecommerce.model.dto.cartItem.Cart;
+import com.brainstation23.ecommerce.ecommerce.model.dto.cartItem.CartItemCreateUpdateRequest;
 import com.brainstation23.ecommerce.ecommerce.persistence.entity.UserEntity;
 import com.brainstation23.ecommerce.ecommerce.service.interfaces.CartItemService;
 import com.brainstation23.ecommerce.ecommerce.service.interfaces.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
+import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
@@ -29,16 +28,18 @@ public class CartController {
     private static final String ATTRIBUTE_PAGE_TITLE = "pageTitle";
     private static final String ATTRIBUTE_CONTENT = "content";
 
-    private final CartItemService cartItemService;
+    private static final String CART_ATTRIBUTE = "cart";
 
     private final CartItemMapper cartItemMapper;
 
     private final UserService userService;
 
+    private final CartItemService cartItemService;
+
 
     @GetMapping
-    public String cartHome(@RequestHeader(value = HttpHeaders.REFERER, required = false) final String referrer, Model model) {
-        String redirectUrl = (referrer == null) ? "/landingpage" : referrer;
+    public String allCartItems(@RequestHeader(value = HttpHeaders.REFERER, required = false) final String referrer, Model model) {
+        String redirectUrl = "/landingpage";
         model.addAttribute("previousUrl", redirectUrl);
         model.addAttribute(ATTRIBUTE_PAGE_TITLE, "Cart");
         model.addAttribute(ATTRIBUTE_CONTENT, "user/usercrud/cart");
@@ -48,15 +49,24 @@ public class CartController {
 
         User user = userService.getOne(userSession.getId());
         var cartItemList = user.getCartItems();
-        var cartItemResponse = cartItemList.stream().map(cartItemMapper::domainToResponse).toList();
-        model.addAttribute("cart_items", cartItemResponse);
+        var cartItemResponseList = cartItemList.stream().map(cartItemMapper::domainToResponse).toList();
+
+        Cart cart = new Cart().setCartItemList(cartItemResponseList);
+        model.addAttribute(CART_ATTRIBUTE, cart);
         return USER_BASE;
     }
-    private Pageable getDefaultProductPage() {
-        return PageRequest.of(0, 20, Sort.by(
-                Sort.Order.asc("date"),
-                Sort.Order.desc("id")));
+
+    @PostMapping("/update")
+    public String updateCartItem(@ModelAttribute CartItemCreateUpdateRequest updatedItem) {
+        updatedItem.setDate(new Timestamp(System.currentTimeMillis()));
+        cartItemService.updateOne(updatedItem);
+        return "redirect:/user/cart";
     }
 
+    @GetMapping("/delete/{id}")
+    public String deleteCartItem(@PathVariable(value = "id") UUID id) {
+        cartItemService.deleteOne(id);
+        return "redirect:/user/cart";
+    }
 
 }
---
+++ src/main/java/com/brainstation23/ecommerce/ecommerce/model/dto/cartItem/Cart.java
@@ -0,0 +1,31 @@
+package com.brainstation23.ecommerce.ecommerce.model.dto.cartItem;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cart {
+
+    private List<CartItemResponse> cartItemList;
+
+    public BigDecimal calculateTotal(){
+        BigDecimal total = BigDecimal.ZERO;
+        for (CartItemResponse item : cartItemList){
+            BigDecimal unitPrice = item.getProduct().getUnitPrice();
+            int quantity = item.getQuantity();
+            total = total.add(unitPrice.multiply(BigDecimal.valueOf(quantity)));
+        }
+        return total;
+    }
+}
